76f3474b30b393257cb51e15ca1d34938dd6c097
Eva Gyomrey <evamargy@gmail.com>
2018-05-16 15:44:13 +0200

    * MyPictures section displays the pictures liked by the user in the Home page and the number of pictures liked so far.

--- src/containers/MyPictures/MyPictures.js
+++ src/containers/MyPictures/MyPictures.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
+import Loader from '../../components/UI/Loader/Loader';
+import Picture from '../../components/Picture/Picture';
+import * as actions from '../../store/actions/index';
 import { connect } from 'react-redux';
+
+import './MyPictures.css';
 
 class MyPictures extends Component {
 
+	componentWillMount () {
+		// Bring the pictures which are in the LocalStorage and put them in the state.
+		this.props.showLikedPictures();
+	}
+
 	render () {
-		console.log(this.props.likedPictures);
+		let photosToDisplay = null;
+		if (this.props.likedPictures) {
+			const pictures = this.props.likedPictures;
+			photosToDisplay = pictures.map( (ob, index) => {
+				return (
+					<div className="PictureCard" key={index}>
+						<Picture
+							sourcePicture={ob}
+							pictureWidth="500"
+							pictureHeight="500"
+						/>
+					</div>
+				);
+			});
+		}
+
 		return (
-			<div>
-				<h1>MY PICTURES</h1>
+			<div className="MyPictures">
+				<p>{this.props.likedPictures.length === 0 ? "You haven't liked any picture yet!" : "Number of pictures liked so far: "+this.props.likedPictures.length}</p>
+				<p></p>
+				<div className="PicturesDisplayed">
+					{!photosToDisplay ? <Loader defaultColor /> : photosToDisplay}
+				</div>
 			</div>
 			);
 	}
@@ -21,6 +50,12 @@ const mapStateToProps = state => {
   };
 }
 
+const mapDispatchToProps = dispatch => {
+    return {
+        showLikedPictures: (pictureSource) => dispatch(actions.addPictureToFavorites())
+    };
+};
 
 
-export default connect(mapStateToProps)(MyPictures);
+
+export default connect(mapStateToProps,mapDispatchToProps)(MyPictures);
--- src/store/actions/pictures.js
+++ src/store/actions/pictures.js
@@ -2,10 +2,14 @@ import * as actionTypes from "./actionTypes";
 
 export const addPictureToFavorites = pictureSource => {
 	var myPictures = JSON.parse(localStorage.getItem("myPictures") || "[]");
-	myPictures.push(pictureSource);
-	localStorage["myPictures"] = JSON.stringify(myPictures);
+	if (pictureSource) {
+		myPictures.push(pictureSource);
+		localStorage["myPictures"] = JSON.stringify(myPictures);
+	}
 	return {
 		type: actionTypes.ADD_PICTURE_TO_FAVORITES,
 		pictureSource: myPictures
 	};
 };
+
+
