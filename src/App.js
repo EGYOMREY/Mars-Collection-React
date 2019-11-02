334945490b5e310fe535d7ac1b5ddcc817e654ff
egyomrey <evamargy@gmail.com>
2019-11-03 00:49:41 +0100

    Update react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
 
-import Navigation from './components/Navigation/Navigation';
-import Home from './containers/Home/Home';
-import MyPictures from './containers/MyPictures/MyPictures';
-import LearnMore from './containers/LearnMore/LearnMore';
+import Navigation from "./components/Navigation/Navigation";
+import Home from "./containers/Home/Home";
+import MyPictures from "./containers/MyPictures/MyPictures";
+import LearnMore from "./containers/LearnMore/LearnMore";
 
-
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from "react-router-dom";
 
 const App = () => (
-      <div>
-        <Navigation />
-      <Switch>
-        <Route path="/learnmore"  component={LearnMore} />
-        <Route path="/mypictures"  component={MyPictures} />
-        <Route path="/" exact component={Home} />
-        <Redirect to="/" />
-      </Switch>
-      </div>
-    );
-
+  <div>
+    <Navigation />
+    <Switch>
+      <Route path="/learnmore" component={LearnMore} />
+      <Route path="/mypictures" component={MyPictures} />
+      <Route path="/" exact component={Home} />
+      <Redirect to="/" />
+    </Switch>
+  </div>
+);
 
 export default App;
